43e0b09f989cfe9c7506c15f01d9f6daa6eaef05
BikromRoy1 <bikrom@peoplentech.com.bd>
2024-07-03 18:45:28 +0600

    Header slider add

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,6 +166,47 @@
   });
 
   /**
+   * Auto generate the carousel indicators
+   */
+  document
+    .querySelectorAll('.carousel-indicators')
+    .forEach((carouselIndicator) => {
+      carouselIndicator
+        .closest('.carousel')
+        .querySelectorAll('.carousel-item')
+        .forEach((carouselItem, index) => {
+          if (index === 0) {
+            carouselIndicator.innerHTML += `<li data-bs-target="#${
+              carouselIndicator.closest('.carousel').id
+            }" data-bs-slide-to="${index}" class="active"></li>`;
+          } else {
+            carouselIndicator.innerHTML += `<li data-bs-target="#${
+              carouselIndicator.closest('.carousel').id
+            }" data-bs-slide-to="${index}"></li>`;
+          }
+        });
+    });
+
+  /**
+   * Init swiper sliders
+   */
+  function initSwiper() {
+    document.querySelectorAll('.init-swiper').forEach(function (swiperElement) {
+      let config = JSON.parse(
+        swiperElement.querySelector('.swiper-config').innerHTML.trim()
+      );
+
+      if (swiperElement.classList.contains('swiper-tab')) {
+        initSwiperWithCustomPagination(swiperElement, config);
+      } else {
+        new Swiper(swiperElement, config);
+      }
+    });
+  }
+
+  window.addEventListener('load', initSwiper);
+
+  /**
    * Navmenu Scrollspy
    */
   let navmenulinks = document.querySelectorAll('.navmenu a');
@@ -192,7 +233,6 @@
   window.addEventListener('load', navmenuScrollspy);
   document.addEventListener('scroll', navmenuScrollspy);
 
-  
   // custom coursor
   var cursor = document.querySelector('.custom-cursor__cursor');
   var cursorinner = document.querySelector('.custom-cursor__cursor-two');
